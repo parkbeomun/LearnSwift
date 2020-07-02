99c1db4e941026bd1f7d04703c5a316376e78156
parkbeomun <sos891214@gmail.com>
2020-07-02 23:42:08 +0900

    first commit

---
+++ LearnSwift.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello to Swift")
+/*:
+ # Welcome to Playgrounds
+ This is your *first* playgound which is intended to demonstrate:
+ * The use of  **Quick Look**
+ * Placing results **in-line&** with the code
+ */
+
+var x = 10
+
+for index in 1...20 {
+    let y = index * x
+    x -= 1
+    print(y)
+}
+
+//: 이것은 한 줄 문서의 텍스트다.
+
+/*:
+이것은 여러 줄로 구성된
+ 문서 텍스트 블록이다.
+ */
+
